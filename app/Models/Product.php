e42d0cd73854f5315eadc39ebbfc3402022d44ac
Indra Jatmiko <indrajatmiko@gmail.com>
2025-10-09 12:58:43 +0700

    v0.6.7 urutan gambar produk

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,7 +19,7 @@ class Product extends Model
      */
     public function images(): HasMany
     {
-        return $this->hasMany(ProductImage::class);
+        return $this->hasMany(ProductImage::class)->oldest();
     }
 
     /**
